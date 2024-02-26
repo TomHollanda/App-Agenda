6d414ccf69477b7c950641a43c3a6de0cdfe988d
TomHollanda <eltonhollanda@gmail.com>
2024-02-26 15:50:28 -0300

    add função salvar

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
+const form = document.getElementById('form-registre');
+const nameList = [];
+const fone = [];
+const address = [];
+const email = [];
+let lines = '';
 const btn = document.querySelector(".place-order");
 let isAnimating = false;
+
+function addline() {
+    const inputContactname = document.getElementById('contactname');
+    const inputContact = document.getElementById('contact');
+
+    if (nameList.includes(inputContactname.value)) {
+        alert(`${inputContactname.value} 'Already an existing contact`);
+    } else {
+        nameList.push(inputContactname.value);
+        fone.push(inputContact.value);
+
+        let linha = '<tr>';
+        linha += `<td>${inputContactname.value}</td>`;
+        linha += `<td>${inputContact.value}</td>`;
+        lines += linha;
+    }
+
+    inputContactname.value = '';
+    inputContact.value = '';
+}
+
+function updateTable() {
+    const tablebody = document.querySelector('tbody');
+    tablebody.innerHTML = lines;
+}
 
 btn.addEventListener("click", () => {
     if (isAnimating) {
         return; // Impede o clique se a animação estiver em andamento
     }
-    
+    addline();
+    updateTable();
+
     isAnimating = true; // Define o estado de animação como verdadeiro
     
     btn.classList.remove("place-order--default");
@@ -16,9 +49,14 @@ btn.addEventListener("click", () => {
         btn.classList.add("place-order--done");
         
         setTimeout(() => {
-        btn.classList.remove("place-order--done");
-        btn.classList.add("place-order--default");
-        isAnimating = false; // Define o estado de animação como falso após a animação ser concluída
-        }, 2000); // Reduzi o tempo do segundo setTimeout para 2000 para corresponder ao tempo de animação do CSS
+            btn.classList.remove("place-order--done");
+            btn.classList.add("place-order--default");
+            isAnimating = false; // Define o estado de animação como falso após a animação ser concluída, reabilita o botão
+        }, 2000); 
     }, 4000);
 });
+
+function addlinha() {
+    const inputNomeAtividade = document.getElementById('contactname');
+    const inputNotaAtividade = document.getElementById('contact');
+}
